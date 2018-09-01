7bec9df868f1a1c71de3b092df72adc18465c932
Eric Dobbs <edobbs@newrelic.com>
2018-09-01 17:08:12 -0600

    Update install.sh with idempotency and change in homebrews
    don't install emacs
    do install duti

--- install.sh
+++ install.sh
@@ -7,14 +7,14 @@ main() {
   # TODO emacs install packages
   install-font
   install-zgen
-  set-shell-to-zsh
+  install-shell
   install-dotfiles
 }
 
 install-brews() {
   BREWS='aspell
 chezscheme
-emacs
+duti
 gnupg
 graphviz
 jq
@@ -31,20 +31,59 @@ zsh'
 }
 
 install-font() {
-  curl -sSL -o /tmp/hack.zip \
-      https://github.com/source-foundry/Hack/releases/download/v3.003/Hack-v3.003-ttf.zip
-  unzip -j -d /Library/Fonts/ /tmp/hack.zip
+  local SOURCE=https://github.com/source-foundry/Hack/releases/download/v3.003/Hack-v3.003-ttf.zip
+  local TARGET_DIR=/Library/Fonts
+  local ARCHIVE=/tmp/hack.zip
+  ls $TARGET_DIR | grep -iq Hack || {
+    test -f $ARCHIVE || curl -sSL -o $ARCHIVE $SOURCE
+    unzip -j -d $TARGET_DIR $ARCHIVE
+  }
 }
 
 install-zgen() {
-  git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
+  local ZGEN="${HOME}/.zgen"
+  test -d $ZGEN && (cd $ZGEN; git rev-parse --git-dir > /dev/null 2>&1) || {
+    git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
+  }
 }
 
-set-shell-to-zsh() {
-  echo /usr/local/bin/zsh | sudo tee -a /etc/shells
-  chsh -s /usr/local/bin/zsh
+install-shell() {
+  local ZSH_PATH=/usr/local/bin/zsh
+  fgrep -q $ZSH_PATH /etc/shells || {
+    echo $ZSH_PATH | sudo tee -a /etc/shells
+  }
+  [ "$SHELL" == "$ZSH_PATH" ] || {
+    chsh -s $ZSH_PATH
+  }
 }
 
 install-dotfiles() {
   stow --target=$HOME home
 }
+
+usage() {
+  cat <<EOF
+Usage: $(basename $0) COMMAND
+  COMMANDs:
+    all
+    brews
+    font
+    zgen
+    shell
+    dotfiles
+
+EOF
+}
+
+readonly CMD=${1:-nothing}
+case $CMD in
+  all)
+    main
+    ;;
+  brews|font|zgen|shell|dotfiles)
+    install-$CMD
+    ;;
+  *)
+    usage
+    ;;
+esac
