9367c6b135fa58cc54432370f770d903ae5600ad
Eric Dobbs <edobbs@newrelic.com>
2018-10-07 15:30:47 -0600

    Show line numbers & ignore javascript semicolons in emacs
    line numbers help when pairing
    modern javascript doesn't need semicolons

--- home/.emacs.d/personal/custom.el
+++ home/.emacs.d/personal/custom.el
@@ -6,7 +6,9 @@
  '(custom-safe-themes
    (quote
     ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+ '(global-display-line-numbers-mode t)
  '(js-indent-level 2)
+ '(js2-strict-missing-semi-warning nil)
  '(org-agenda-files (quote ("~/Documents/GTD.org")))
  '(org-default-notes-file "~/Documents/GTD.org")
  '(org-habit-following-days 14)
@@ -19,7 +21,6 @@
    (quote
     (async bind-key counsel f flx gh ghub git-commit haml-mode helm-core ht json-reformat json-snatcher logito magit-popup makey marshal memoize org-clock-convenience pcache pkg-info popup pythonic rich-minority s swiper use-package vmd-mode with-editor anaconda-mode avy coffee-mode company company-anaconda elisp-slime-nav flx-ido geiser helm helm-ag helm-descbinds helm-projectile hl-todo ido-completing-read+ inf-ruby js2-mode json-mode rainbow-delimiters scss-mode smex web-mode yaml-mode yari solarized-theme graphviz-dot-mode ivy csv-mode dockerfile-mode markdown-mode vkill exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree smartrep smartparens smart-mode-line operate-on-number move-text magit projectile ov imenu-anywhere guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major dash crux browse-kill-ring beacon anzu ace-window)))
  '(sh-basic-offset 2)
- '(sh-indentation 2)
  '(solarized-high-contrast-mode-line nil)
  '(web-mode-code-indent-offset 2)
  '(web-mode-markup-indent-offset 2))
