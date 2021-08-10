aecf67974c764aa079512bf131e081184f713858
Eric Dobbs <eric@dobbse.net>
2021-08-10 12:19:29 -0400

    style update in custom.el: (quote) replaced with '

--- home/.emacs.d/personal/custom.el
+++ home/.emacs.d/personal/custom.el
@@ -4,23 +4,21 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("0598c6a29e13e7112cfbc2f523e31927ab7dce56ebb2016b567e1eff6dc1fd4f" "d91ef4e714f05fff2070da7ca452980999f5361209e679ee988e3c432df24347" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+   '("0598c6a29e13e7112cfbc2f523e31927ab7dce56ebb2016b567e1eff6dc1fd4f" "d91ef4e714f05fff2070da7ca452980999f5361209e679ee988e3c432df24347" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default))
  '(epa-pinentry-mode 'loopback)
+ '(epg-pinentry-mode 'loopback)
  '(js-indent-level 2)
  '(js2-strict-missing-semi-warning nil)
- '(org-agenda-files (quote ("~/Documents/GTD.org")))
+ '(org-agenda-files '("~/Documents/GTD.org"))
  '(org-default-notes-file "~/Documents/GTD.org")
  '(org-habit-following-days 14)
  '(org-habit-graph-column 32)
  '(org-habit-preceding-days 120)
  '(org-habit-show-all-today t)
  '(org-modules
-   (quote
-    (org-bbdb org-bibtex org-docview org-gnus org-habit org-info org-irc org-mhe org-rmail org-w3m)))
+   '(org-bbdb org-bibtex org-docview org-gnus org-habit org-info org-irc org-mhe org-rmail org-w3m))
  '(package-selected-packages
-   (quote
-    (typescript-mode swiper pythonic ruby-tools smart-mode-line transient with-editor projectile rainbow-mode smartparens super-save web-mode which-key zenburn-theme ace-window anaconda-mode anzu async avy beacon bind-key browse-kill-ring coffee-mode company company-anaconda company-lsp counsel crux csv-mode dash diff-hl diminish discover-my-major dockerfile-mode easy-kill editorconfig elisp-slime-nav epl exec-path-from-shell expand-region f flx flx-ido flycheck geiser gh ghub gist git-commit git-timemachine gitconfig-mode gitignore-mode god-mode graphviz-dot-mode grizzl groovy-mode guru-mode haml-mode helm helm-ag helm-core helm-descbinds helm-projectile hl-todo ht ido-completing-read+ imenu-anywhere inf-ruby ivy js2-mode json-mode json-reformat json-snatcher logito lsp-ui magit magit-popup makey markdown-mode marshal memoize move-text operate-on-number org-clock-convenience ov pcache pkg-info popup rainbow-delimiters rich-minority s scss-mode smartrep smex solarized-theme undo-tree vkill vmd-mode volatile-highlights yaml-mode yari zop-to-char)))
+   '(typescript-mode swiper pythonic ruby-tools smart-mode-line transient with-editor projectile rainbow-mode smartparens super-save web-mode which-key zenburn-theme ace-window anaconda-mode anzu async avy beacon bind-key browse-kill-ring coffee-mode company company-anaconda company-lsp counsel crux csv-mode dash diff-hl diminish discover-my-major dockerfile-mode easy-kill editorconfig elisp-slime-nav epl exec-path-from-shell expand-region f flx flx-ido flycheck geiser gh ghub gist git-commit git-timemachine gitconfig-mode gitignore-mode god-mode graphviz-dot-mode grizzl groovy-mode guru-mode haml-mode helm helm-ag helm-core helm-descbinds helm-projectile hl-todo ht ido-completing-read+ imenu-anywhere inf-ruby ivy js2-mode json-mode json-reformat json-snatcher logito lsp-ui magit magit-popup makey markdown-mode marshal memoize move-text operate-on-number org-clock-convenience ov pcache pkg-info popup rainbow-delimiters rich-minority s scss-mode smartrep smex solarized-theme undo-tree vkill vmd-mode volatile-highlights yaml-mode yari zop-to-char))
  '(sh-basic-offset 2)
  '(solarized-high-contrast-mode-line nil)
  '(tab-width 2)
